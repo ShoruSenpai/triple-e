1a7c225ea0dd73ffaf5a85a48d4e7d538fa7af94
ShoruSenpai <shorusenpai@gmail.com>
2026-09-02 09:34:33 +0700

    push new file: commit my file

---
+++ lib/yusky/testing.dart
@@ -0,0 +1,12 @@
+void main() {
+    var firstName;
+    var lastName;
+    var fullName;
+    
+    firstName = 'Shoru';
+    lastName = 'Senpai';
+
+    fullName = firstName + ' ' + lastName;
+
+    print(fullName);
+}
